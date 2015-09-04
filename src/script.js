8718c5ec1119c9ca2ec7c5fd0e56eaaf254cea9e
Bo Sun <bsun@axioma.com>
2015-09-04 13:55:35 -0400

    add p3

--- src/script.js
+++ src/script.js
@@ -1,33 +1,33 @@
-var users = [{id:'1', name:'Alex'}, {id:'2', name:'John'}];
+var users = [{id: '1', name: 'Alex'}, {id: '2', name: 'John'}];
+
 function getUserById(id) {
-  return new Promise((resolve,reject)=>{
-      setTimeout(function(){
-        var matched={};
-        for(var i=0;i<users.length;i++) {
-          if(users[i].id===id) {
-            matched =users[i];
-            break;
-          }
+  return new Promise((resolve, reject)=> {
+    setTimeout(function () {
+
+      if (typeof id !== 'string') reject('id must be string');
+
+      var matched = {};
+      for (var i = 0; i < users.length; i++) {
+        if (users[i].id === id) {
+          matched = users[i];
+          break;
         }
-        resolve(matched);
-      }, 2000);
-})
+      }
+      resolve(matched);
+    }, 2000);
+  })
 }
 
-var p1 = getUserById('2'), p2= getUserById('1');
-Promise.all([p1,p2])
-  .then((data)=>console.log(data))
-.catch(err=>{
-  console.log(err);
-})
+var p1 = getUserById('2'), p2 = getUserById('1'), p3 = getUserById(3);
+Promise.all([p1, p2])
+  .then((data)=> {
+    console.log(data);
+    return p3;
+  })
+  .catch(err=> {
+    console.log(err);
+  });
 
 
-function factorial(n, acc = 1) {
-  if (n <= 1) return acc;
-  return factorial(n - 1, n * acc);
-}
 
-// Stack overflow in most implementations today,
-// but safe on arbitrary inputs in ES2015
-console.log(factorial(1000));
 

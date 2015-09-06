e0952186553ee20e3ca9ad427778114b3fd63e27
bo sun <bsun@axioma.com>
2015-09-05 20:17:30 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+// this is only used to generate output file
 babel script.js --modules common --source-maps --watch --out-file script-compiled.js
+// typical setup
+nodemon server.js 
+	where server.js is es5 and server.js requires script.js, and script.js can be es6
